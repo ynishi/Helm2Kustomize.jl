325905f7f6da39d3a133d583aee239f667485205
Yutaka Nishimura <ytk.nishimura@gmail.com>
2023-01-29 11:00:02 +0900

    impl init

--- src/Helm2Kustomize.jl
+++ src/Helm2Kustomize.jl
@@ -5,12 +5,14 @@ using OrderedCollections
 using Comonicon
 using UUIDs
 
-Base.@kwdef struct Config
+export Config, init
+
+Base.@kwdef mutable struct Config
     outdir::String = ""
     tempdir::String = ""
     helm_repository::String = ""
     helm_version::String = ""
-    helm_options::Dict = Dict()
+    helm_options::Vector{Dict} = []
     templatepath::String = ""
     templatedir::String = ""
     dict::Dict{Symbol,Any} = Dict()
@@ -22,9 +24,71 @@ end
 const KUSTOMIZATION_FILENAME = "kustomization.yaml"
 CONFIG = Config()
 
-function init(repo, outdir, version, templatepath, force, config, base, keep)
+function init(
+    repository::String,
+    outdir::String,
+    version::String,
+    templatepath::String,
+    configfile::String,
+    force::Bool,
+    base::Bool,
+    keep::Bool)
+
     @debug "init"
-    Config()
+    config::Config = Config()
+
+    if repository == ""
+        throw(error("empty repo"))
+    end
+
+    config.helm_repository = repository
+
+    splitted = split(config.helm_repository, "/")
+    if length(splitted) == 0
+        throw(error("invalid repo name"))
+    end
+    repolast = last(splitted)
+
+    config.outdir = outdir == "" ? "$(repolast)_out" : outdir
+
+    config.isforce = force
+
+    if !config.isforce &&
+       (isdir(config.outdir) || isfile(config.outdir))
+        throw(error("out dir exists $OUT_DIR`"))
+    end
+
+    config.helm_version = version
+
+    config.templatepath = templatepath == "" ? repolast : templatepath
+
+    config.tempdir = gettemppath()
+    if config.tempdir == ""
+        throw(error("failed to create temp dir name"))
+    end
+    config.templatedir = joinpath(config.tempdir, config.templatepath, "templates")
+
+    if isfile(configfile)
+        config.dict = YAML.load_file(configfile, dicttype=Dict{Symbol,Any})
+    end
+    if haskey(config.dict, :helm) && haskey(config.dict[:helm], :options)
+        config.helm_options = config.dict[:helm][:options]
+    end
+
+    config.isbase = base
+    config.iskeep = keep
+
+    @debug config
+    config
+end
+
+function gettemppath()
+    joinpath(pwd(), "tmp_" * string(uuid1()))
+end
+
+function up()
+    @debug "up"
+    mkpath(config.tempdir)
 end
 
 function down()
@@ -43,12 +107,13 @@ function mv_kustomize()
     @info "mv_kustomize"
 end
 
-@main function helm2kustomize(repo::String; outdir::String="", version::String="", templatepath::String="", config::String="config.yaml", force::Bool=false, base::Bool=false, keep::Bool=false)
+@main function helm2kustomize(repo::String; outdir::String="", version::String="", templatepath::String="", configfile::String="config.yaml", force::Bool=false, base::Bool=false, keep::Bool=false)
 
     p = pwd()
 
     try
-        global CONFIG = init(repo, outdir, version, templatepath, force, config, base, keep)
+        global CONFIG = init(repository, outdir, version, templatepath, configfile, force, base, keep)
+        up()
         make_template()
         make_kustomize()
         cd(p)
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,37 @@ using Helm2Kustomize
 using Test
 
 @testset "Helm2Kustomize.jl" begin
-    # Write your tests here.
+    @testset "init" begin
+        repository = "group/repository"
+        outdir = "tmp_dir"
+        version = "1.0.0"
+        templatepath = "templatepath"
+        configfile = "configtest.yaml"
+        base = true
+        keep = true
+        force = false
+        config = init(repository, outdir, version, templatepath, configfile, force, base, keep)
+
+        @test config.outdir == "tmp_dir"
+        @test config.tempdir != ""
+        @test config.helm_repository == "group/repository"
+        @test config.helm_version == "1.0.0"
+        @test length(keys(config.helm_options)) == 1
+        @test config.helm_options[1][:name] == "optionName"
+        @test config.helm_options[1][:value] == "optionValue"
+        @test config.templatepath == "templatepath"
+        @test config.templatedir == "$(config.tempdir)/templatepath/templates"
+        @test length(keys(config.dict)) == 1
+        @test config.isforce == false
+        @test config.isbase
+        @test config.iskeep
+
+        config2 = init(repository, outdir, version, "", configfile, force, base, keep)
+        @test config2.templatedir == "$(config2.tempdir)/repository/templates"
+
+        config3 = init(repository, outdir, version, templatepath, "", force, base, keep)
+        @error config3
+        @test length(keys(config3.helm_options)) == 0
+        @test length(keys(config3.dict)) == 0
+    end
 end
